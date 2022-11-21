8ad9f3379b8f057ab74cae53d065a86a8f2f83d2
wilbuuuur <ville.aalto@myy.haaga-helia.fi>
2022-11-21 15:21:08 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Vaatelist from './components/Vaatteet';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position='static'>
+        <Toolbar> 
+          <Typography variant='h6'>
+            Vaate
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Vaatelist />
     </div>
   );
 }
