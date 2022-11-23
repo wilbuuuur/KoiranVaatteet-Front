2f8c53bc3d064dcb05cd016b1cddc7a613f8161a
wilbuuuur <ville.aalto@myy.haaga-helia.fi>
2022-11-23 17:48:28 +0200

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
-export const API_URL ="https://koiranvaatetesti.herokuapp.com/rest/vaatteet"
+export const API_URL ="https://koiranvaatetesti.herokuapp.com/api/vaates/"
+export const API_URL_VAATTEET ="https://koiranvaatetesti.herokuapp.com/rest/vaatteet"
 export const API_URL_VALMISTAJAT ="https://koiranvaatetesti.herokuapp.com/rest/valmistajat"
